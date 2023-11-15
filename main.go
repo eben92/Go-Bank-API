4715d07f32aa756adf93e0b34e466f019bcf4ca7
rhutmann <pappy.rhutmannjnrolege@gmail.com>
2023-11-15 05:26:43 +0000

    Add storage interface and Postgres implementation

--- main.go
+++ main.go
@@ -1,6 +1,18 @@
 package main
 
+import (
+	"log"
+)
+
 func main() {
-	server := NewAPIServer(":3000")
+	store, err := NewPostgresStore()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// fmt.Printf("%+v\n", store)
+
+	server := NewAPIServer(":3000", store)
 	server.Run()
 }
